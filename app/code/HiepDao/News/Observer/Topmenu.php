2e2ff4e43f499b894ea261fafb84b25fd8f9740a
wsl-ubuntu <wsl@ubuntu.test>
2023-05-19 10:20:57 +0700

    add links to menu

---
+++ app/code/HiepDao/News/Observer/Topmenu.php
@@ -0,0 +1,24 @@
+<?php
+namespace HiepDao\News\Observer;
+use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Framework\Data\Tree\Node;
+use Magento\Framework\Event\ObserverInterface;
+class Topmenu implements ObserverInterface
+{
+    public function execute(EventObserver $observer)
+    {
+        $menu = $observer->getMenu();
+        $tree = $menu->getTree();
+        $data = [
+            'name'      => __('News'),
+            'id'        => 'news',
+            'url'       => 'news',
+            'is_active' => false
+        ];
+        $node = new Node($data, 'id', $tree, $menu);
+        $menu->addChild($node);
+        return $this;
+    }
+}
+ 
+?>
